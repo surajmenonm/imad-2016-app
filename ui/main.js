3702c9ce69dbdf7d5ad7467551f153cf76a591aa
surajmenonm <surajmenonm@gmail.com>
2016-09-26 23:57:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,30 @@
 //counter
 var button=document.getElementById('counter');
-var counter=0;
+
 
 button.onclick = function(){
-  //make a request
+  //create a object request
+  var request= new XMLHttpRequest();
   
   //capture response
-  
+  request.onreadystateChange = function(){
+      if(request.readystate===XMLHttpRequest.DONE)
+      {
+          //TAKE SOME ACTION
+        if (request.status===200) {
+            var counter = request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML= counter.toString();
+        }
+          
+      }
+      //NOT DONE YET
+      
+  };
   //render variable in span
-  counter= counter+1;
-  var span=document.getElementById('count')
-  span.innerHTML= counter.toString();
-  
+ 
+  //make request
+  request.open('GET', "http://surajmenonm.imad.hasura-app.io/counter", true)
+  request.send(null)
     
 };
