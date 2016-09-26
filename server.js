54e1400c0dc38572079a89b24eed1e9f01b73081
surajmenonm <surajmenonm@gmail.com>
2016-09-27 00:49:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+var names =[];
+app.get('/submit-name/:name', function(req, res){
+
+var name=req.params.name;
+names.push(name);
+res.send(JSON.stringify(names));
+    
+})
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
