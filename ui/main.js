e9e593701ec248abe5b90711256729683d90a3c4
surajmenonm <surajmenonm@gmail.com>
2016-09-27 00:34:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,26 @@ button.onclick = function(){
  
   //make request
   request.open('GET', "http://surajmenonm.imad.hasura-app.io/counter", true)
-  request.send(null)
+  request.send(null);
+    
+};
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick= function(){
+    
+    
+    var names=['name1', 'name2', 'name3'];
+    var list='';
+    for (var i=0;i=names.length; i++){
+        list +='<li>'+names[i]+'</li>';
+        
+        
+    }
+    
+    var ul=document.getElementById('namelist')
+    ul.innerHTML=list;
+    
     
 };
